a4d843bf54f182c51a93306fa203da5917491b61
yukselibrahim <113780430+yukselibrahim@users.noreply.github.com>
2022-11-01 13:50:26 +0300

    C#
    multiplication table

---
+++ Multiplication Table/Alıştırma 9/Islem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Alıştırma_9
+{
+    public class Islem
+    {
+
+        public  void CarpimTablosu()
+        {
+            for (int i = 1; i <=10; i++)
+            {
+                Console.WriteLine("********");
+                for (int j = 1; j <=10; j++)
+                {
+
+                    Console.WriteLine(i+"x"+j+"="+(i*j));
+                }
+
+            }
+
+        }
+
+       
+        
+        
+
+
+
+    }
+}
---
+++ Multiplication Table/Alıştırma 9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alıştırma_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Islem carpimTablosu = new Islem();
+            carpimTablosu.CarpimTablosu();
+
+
+
+          
+
+            Console.ReadLine();
+
+        }
+    }
+}
